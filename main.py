778fd86b379283cc634b310af002b7ccf7ca8b76
Koval <kovalevice=artefat@gmail.com>
2023-10-10 17:33:24 +0300

    Fix get_suitable_users

--- main.py
+++ main.py
@@ -66,75 +66,6 @@ async def get_age(message: types.Message, state: FSMContext):
     await message.reply(f"Готовься к поиску путник!!!")
 
 available_users = {}
-
-# @dp.chat_member()
-# async def on_chat_member_join(message: types.ChatMemberUpdated):
-#     user_id = message.new_chat_member.user.id
-#     if user_id != bot.id:  # Игнорируем событие, если это вход бота в чат
-#         available_users.append(user_id)
-#         print(f"Добавлен новый пользователь. Список доступных пользователей: {available_users}")
-#
-#
-# # Хэндлер на событие выхода пользователя из чата
-# @dp.chat_member()
-# async def on_chat_member_leave(message: types.ChatMemberUpdated):
-#     user_id = message.left_chat_member.user.id
-#     if user_id != bot.id:  # Игнорируем событие, если это выход бота из чата
-#         available_users.remove(user_id)
-#
-
-# @form_router.message(Form.MATCHED)
-# async def handle_matched_chat(message: types.Message, state: FSMContext):
-#     chat_id = message.chat.id
-#     if chat_id in active_chats:
-#         partner_chat_id = active_chats[chat_id]
-#         if partner_chat_id:
-#             await bot.send_message(partner_chat_id, message.text)
-#         else:
-#             await message.reply("Ожидание собеседника...")
-#     else:
-#         await message.reply("Чат закончен.")
-#         await state.finish()
-
-
-# @dp.message(Command('search'))
-# async def cmd_search(message: types.Message, state: FSMContext):
-#     chat_id = message.chat.id
-#
-#     if chat_id in active_chats:
-#         await message.reply("Вы уже ищите!")
-#         return
-#
-#
-#
-#     user_data = await state.get_data()
-#     age = user_data.get("age")
-#
-#     available_users[f"{message.chat.id}"] = age
-#
-#     if age is None:
-#         await message.reply("Сначала укажите свой возраст с помощью команды /start")
-#         return
-#
-#     suitable_users = get_suitable_users(age)
-#     if suitable_users:
-#         if str(chat_id) in suitable_users:
-#             suitable_users.remove(str(chat_id))  # Удаляем текущего пользователя из списка suitable_users
-#
-#         if suitable_users:
-#             partner_chat_id = random.choice(suitable_users)  # Выбираем случайного подходящего пользователя
-#             suitable_users.remove(partner_chat_id)  # Удаляем выбранного пользователя из списка suitable_users
-#
-#             active_chats[chat_id] = partner_chat_id
-#             active_chats[partner_chat_id] = chat_id
-#             await state.set_state(Form.MATCHED)
-#             await message.reply("Собеседник найден! Можете начинать общение.")
-#         else:
-#             await message.reply("Нет доступных собеседников. Ожидайте...")
-#     else:
-#         await message.reply("Нет доступных собеседников. Ожидайте...")
-
-
 async def check_available_partners(chat_id, state):
     while True:
         suitable_users = get_suitable_users(available_users[chat_id][1])
@@ -182,25 +113,9 @@ def get_suitable_users(age):
     suitable_users = []
     for user_id, user_data in available_users.items():
         user_age = user_data[1]
-        if user_age is not None and abs(int(user_age[1]) - int(age)) <= 5:
+        if user_age is not None and abs(int(user_age) - int(age)) <= 5:
             suitable_users.append(user_id)
     return suitable_users
-
-# @form_router.message(Form.MATCHED)
-# async def handle_matched_chat(message: types.Message, state: FSMContext):
-#     chat_id = message.chat.id
-#     partner_chat_id = active_chats.get(chat_id)
-#
-#     if partner_chat_id is None:
-#         await message.reply("Вы не в активном чате.")
-#         return
-#
-#     partner_age = available_users.get(partner_chat_id)
-#     if partner_age is None:
-#         await message.reply("Возраст партнера неизвестен.")
-#     else:
-#         await message.reply(f"Возраст вашего партнера: {partner_age} лет.")
-#
 
 
 @form_router.message(Form.MATCHED)
